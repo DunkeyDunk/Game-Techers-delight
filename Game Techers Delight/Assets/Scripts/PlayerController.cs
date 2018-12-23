54a503d8a3e9837a9a26f5693b54419f5f86a304
Goggon <bettemoller@hotmail.com>
2018-12-23 17:24:41 +0100

    Basic Movement Controller
    can move in 8 directions, diagonal might be faster for some reason. Walls can be collided with aswell

---
+++ Game Techers Delight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private float moveInputx;
+    private float moveInputy;
+
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveInputx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInputx * speed, rb.velocity.y);
+
+        moveInputy = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, moveInputy * speed);
+    }
+}
